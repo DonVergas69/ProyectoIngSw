a7505904b0202d71f2ff8de4b2bda16bbfb7b1b1
lenovo <lenovo@192.168.0.50>
2025-10-23 20:19:33 -0600

    Creacion de la clase Usuario

--- src/pck_proyecto/Usuario.java
+++ src/pck_proyecto/Usuario.java
@@ -9,5 +9,51 @@ package pck_proyecto;
  * @author lenovo
  */
 public class Usuario {
+    protected int idUsuario;
+    protected String nombre;
+    protected String rol;
+    protected String contrasena;
+
+    public Usuario(int idUsuario, String nombre, String rol, String contrasena) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.rol = rol;
+        this.contrasena = contrasena;
+    }
     
+    public Usuario(){
+        this(0,null,null,null);
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
 }
