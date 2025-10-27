a04c8d2986ffb78cbdbf1b11960e00d82b20b82e
User <User@192.168.0.195>
2025-10-27 01:18:18 -0600

    se inlcuyeron mensajes de error y excepciones adicionales al metodo conexion

--- src/pck_proyecto/Conexion_bd.java
+++ src/pck_proyecto/Conexion_bd.java
@@ -10,18 +10,23 @@ package pck_proyecto;
  */
 
 import java.sql.*;
+import javax.swing.JOptionPane;
 public class Conexion_bd {
     
     static String url = "jdbc:mysql://localhost:3306/bd_heladeria";
     static String user = "root";
     static String password = "PacMan_12";
     
-    public static Connection getConection(){
+    public  Connection getConection(){
         Connection con=null;
         try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(url,user,password);
+            JOptionPane.showMessageDialog(null,"Se establecio la conexion con el servidor","Todo bien",JOptionPane.INFORMATION_MESSAGE);
         }catch(SQLException e){
             e.printStackTrace();
+        }catch(ClassNotFoundException e){
+            JOptionPane.showMessageDialog(null,"no se encontro el driver","Error",JOptionPane.ERROR_MESSAGE);
         }
         return con;
     }
